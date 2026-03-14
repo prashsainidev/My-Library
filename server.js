a8c3bd00fa5f1f1cf246839d1d448ad8b61a05ec
PRASHANT SAINI <Prashant.saini.3150@gmail.com>
2026-03-14 12:45:27 +0530

    feat: Configure Vercel deployment and introduce comprehensive UI styling for global elements, header, buttons, forms, authors, and books.

--- server.js
+++ server.js
@@ -44,5 +44,5 @@ app.use('/books', bookRouter)
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running at http://localhost:${PORT}`);
 })
